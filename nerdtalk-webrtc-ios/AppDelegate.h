64720eb1d012fb08ab9ea1d56ed4305bbed0393f
Earl Ferguson (BlueFletch) <earl.ferguson@bluefletch.com>
2015-03-03 16:37:36 -0500

    Objective-c

---
+++ nerdtalk-webrtc-ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  nerdtalk-webrtc-ios
+//
+//  Created by Earl Ferguson (BlueFletch) on 3/3/15.
+//  Copyright (c) 2015 BlueFletch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ nerdtalk-webrtc-ios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  nerdtalk-webrtc-ios
+//
+//  Created by Earl Ferguson (BlueFletch) on 3/3/15.
+//  Copyright (c) 2015 BlueFletch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
